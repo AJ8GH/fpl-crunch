6f3c922098f81f3afdb8454dded764e953fcc5fc
Adam Jonas <adamjonas25@gmail.com>
2024-09-11 16:10:12 +0100

    Tidy up

--- api/src/main/java/io/github/aj8gh/fplcrunch/api/model/response/LoginResponse.java
+++
@@ -1,8 +0,0 @@
-package io.github.aj8gh.fplcrunch.api.model.response;
-
-import lombok.Builder;
-
-@Builder(toBuilder = true)
-public record LoginResponse() {
-
-}
--- api/src/main/java/io/github/aj8gh/fplcrunch/api/resource/AbstractResource.java
+++ api/src/main/java/io/github/aj8gh/fplcrunch/api/resource/AbstractResource.java
@@ -1,9 +1,6 @@
 package io.github.aj8gh.fplcrunch.api.resource;
 
 import io.github.aj8gh.fplcrunch.client.FplApiClient;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -12,8 +9,6 @@ public abstract class AbstractResource {
   @RestClient
   protected FplApiClient client;
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
   protected Response ok(Object object) {
     return Response.ok(object).build();
   }
